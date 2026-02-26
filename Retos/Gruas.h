5ee28adff3aa8c28b675feb04ba35293f1c6ee47
Mario <a25216366@alumnos.uady.mx>
2026-02-26 08:59:27 -0600

    Configurar gitignore y actualizar respuestas

---
+++ Retos/Gruas.h
@@ -0,0 +1,26 @@
+#ifndef GRUAS_H
+#define GRUAS_H
+#include "vehiculo.h"
+
+class Gruas : Vehiculo {
+
+    private:
+    float capacidad_carga;
+    std::string tipo_estabiizador;
+
+    public:
+
+    Gruas::Gruas();
+    Gruas::Gruas()
+
+    void actualizarCapacidad();
+    vi
+
+
+
+}
+
+
+
+
+#endif
