6a3165fcea149ed5800970b6fd05659485d40485
Mario <a25216366@alumnos.uady.mx>
2026-03-14 17:42:06 -0600

    Acctualizacion de doc

---
+++ ejercicio_03_polimorfismo/figura.h
@@ -0,0 +1,22 @@
+#ifndef FIGURAS_H
+#define FIGURAS_H
+#include <string>
+
+class Figura {
+
+    protected: 
+    std::string nombre;
+
+    public:
+    Figura(std::string nombre);
+    virtual ~Figura();
+
+    virtual double calcularArea() = 0;
+    virtual void describir() = 0;
+
+    std::string obtenerNombre();
+
+};
+
+
+#endif
